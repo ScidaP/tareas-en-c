34609233b2d2eb92b879b9aae8f5738950b221e0
ScidaP <patricioscida@gmail.com>
2020-11-19 15:32:23 -0300

    Casi terminado. No funciona la función contarInyectable().

--- TP07-ScidáPatricio/ejercicio1.c
+++ TP07-ScidáPatricio/ejercicio1.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-struct medicamentos {
+typedef struct medicamentos {
     char nombre[40];
     char presentacion[40];
     char laboratorio[40];
@@ -10,57 +10,69 @@ struct medicamentos {
     int stock;
     int fecha_venc;
     char indicacion[40];
-};
+}inventario;
 
-void modificarPrecio(struct medicamentos *medicamento1) {
-    if (medicamento1->stock < 5) {
-        medicamento1->precio *= 1.10; /* Le sumo el 10% */
-    } else {
-        medicamento1->precio *= 0.90; /* Le resto el 10% */
+void contarInyectable(inventario *medicamentos, int cant_inyectable) { /* Función para calcular la cantidad de medicamentos que son inyectables*/
+    char tipo[20] = "Inyectable";
+    if(strcmp(medicamentos->presentacion, tipo) == 0){
+        cant_inyectable++;
     }
 }
 
-void cargarDatos(struct medicamentos *medicamento1) {
+void modificarPrecio(inventario *medicamentos) {
+    if (medicamentos->stock < 5) {
+        medicamentos->precio *= 1.10; /* Le sumo el 10% */
+    } else {
+        medicamentos->precio *= 0.90; /* Le resto el 10% */
+    }
+}
+
+void cargarDatos(inventario *medicamentos) {
     puts("Ingrese el nombre del medicamento.");
     fflush(stdin);
-    gets(medicamento1->nombre);
-    puts("Ingrese la presentacion. ('Capsula, inyectable, gotas, etc')");
+    gets(medicamentos->nombre);
+    puts("Ingrese la presentacion. ('capsula, inyectable, gotas, etc') Sin acentos.");
     fflush(stdin);
-    gets(medicamento1->presentacion);
+    gets(medicamentos->presentacion);
     puts("Ingrese el laboratorio");
     fflush(stdin);
-    gets(medicamento1->laboratorio);
+    gets(medicamentos->laboratorio);
     puts("Ingrese el precio.");
-    scanf("%d", &medicamento1->precio);
+    scanf("%d", &medicamentos->precio);
     puts("Ingrese el stock");
-    scanf("%d", &medicamento1->stock);
+    scanf("%d", &medicamentos->stock);
     puts("Ingrese la fecha de vencimiento, en formato YYYYMMDD");
-    scanf("%d", &medicamento1->fecha_venc);
+    scanf("%d", &medicamentos->fecha_venc);
     fflush(stdin);
     puts("Ingrese la indicacion");
-    gets(medicamento1->indicacion);
+    gets(medicamentos->indicacion);
 }
 
 
-void mostrarDatos(struct medicamentos medicamento1) {
-    printf("El nombre ingresado es: %s\n", medicamento1.nombre);
-    printf("La presentación es: %s\n", medicamento1.presentacion);
-    printf("El laboratorio es: %s\n", medicamento1.laboratorio);
-    printf("El precio es: %d\n", medicamento1.precio);
-    printf("El stock es de: %d\n", medicamento1.stock);
-    printf("La fecha de vencimiento es: %d\n", medicamento1.fecha_venc);
-    printf("La indicacion es: %s\n", medicamento1.indicacion);
+void mostrarDatos(inventario medicamentos) {
+    puts("------------------------");
+    printf("El nombre ingresado es: %s\n", medicamentos.nombre);
+    printf("La presentación es: %s\n", medicamentos.presentacion);
+    printf("El laboratorio es: %s\n", medicamentos.laboratorio);
+    printf("El precio es: %d\n", medicamentos.precio);
+    printf("El stock es de: %d\n", medicamentos.stock);
+    printf("La fecha de vencimiento es: %d\n", medicamentos.fecha_venc);
+    printf("La indicacion es: %s\n", medicamentos.indicacion);
 }
 
 int main() {
-    struct medicamentos medicamento1[20];
-    int cant;
+    inventario medicamentos[20];
+    int cant, cant_inyectable = 0;
     puts("Ingrese de cuantos medicamentos necesita almacenar datos.");
     scanf("%d", &cant);
     for (int i=0; i<cant; i++){
-        cargarDatos(&medicamento1[i]);
-        modificarPrecio(&medicamento1[i]);
-        mostrarDatos(medicamento1[i]);
+        cargarDatos(&medicamentos[i]);
+        modificarPrecio(&medicamentos[i]);
+        contarInyectable(&medicamentos[i], &cant_inyectable);
     }
+    for (int i=0; i < cant; i++) { /* Se mostrarán los datos de todos los medicamentos, después de haberlos ingresado.*/
+        mostrarDatos(medicamentos[i]);
+    }
+    printf("En total, %d medicamentos son inyectables.", cant_inyectable);
     return 0;
 }
