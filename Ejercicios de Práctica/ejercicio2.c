4647ed71ef8fd12f3c876654d6cd70e55c0b0805
ScidaP <patricioscida@gmail.com>
2020-11-30 09:40:33 -0300

    Ejercicio 2 terminado

--- Ejercicios de Práctica/ejercicio2.c
+++ Ejercicios de Práctica/ejercicio2.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 void invertirPalabra(char *pPalabra) {
     char palabraInvertida[30];
-    int j = 0;
-    while (*pPalabra != '\0') {
-        palabraInvertida[j] = pPalabra;
-        j++;
-        pPalabra++;
+    int aux = strlen(pPalabra), i = 0, j = aux;
+    for (int k = 0; k < aux; k++) { // Convierte todas las letras a mayúscula, para que no hayan errores si el usuario ingresa mayusculas.
+            toupper(pPalabra);
+            pPalabra++;
     }
-    puts(palabraInvertida);
+
+    for (i; i < aux; i++) {
+        palabraInvertida[i] = pPalabra[j-1];
+        j--;
+    }
+    palabraInvertida[i] = '\0';
+
+    if (strcmp(palabraInvertida, pPalabra) == 0) {
+        puts("La palabra ingresada es palindroma");
+    } else {
+        puts("La palabra no es palindroma.");
+    }
 }
 
 int main() {
@@ -18,7 +29,6 @@ int main() {
     puts("Ingrese una palabra para invertir");
     fflush(stdin);
     gets(palabra);
-
-    invertirPalabra(&palabra);
+    invertirPalabra(palabra);
     return 0;
 }
